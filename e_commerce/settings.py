95157f137c40cc716e35f9a73614ff86da0d87c7
Misbahkk <misbahyousaf00@gmail.com>
2025-01-17 22:32:38 +0500

    Add vercel project solve erro1r

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(qul&=yg%6&#ube2mq27adl75%a)^r2)v&(^s5r9-zmjzhg$4z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app','.now.sh']
 
 
 # Application definition
