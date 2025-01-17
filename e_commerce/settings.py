73c3e2cb964dabfe3e0ee8249aa8049a7dadbe5a
Misbahkk <misbahyousaf00@gmail.com>
2025-01-17 23:10:51 +0500

    Add vercel project solve tinker

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -82,9 +82,12 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',
     }
 }
 
