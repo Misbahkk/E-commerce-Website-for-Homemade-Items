4e4f6be6a981c2846a314741be4ca2a5c6cca983
Misbahkk <misbahyousaf00@gmail.com>
2025-01-18 18:41:11 +0500

    Add guniric

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://e-commerce-website-for-homemade-production.up.railway.app/',
+]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
